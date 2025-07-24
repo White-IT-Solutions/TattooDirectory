fddcad4b73e3a00e528007ea179c407cac974aa3
artiechokeheart <artemiswilkinson@hotmail.com>
2025-07-24 15:00:57 +0100

    Add new FE and remove old ones

--- frontend-nextjs/app/artist/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import ArtistProfile from '@/components/pages/ArtistProfile'
-
-export default function ArtistProfilePage({ 
-  params 
-}: { 
-  params: { id: string } 
-}) {
-  return <ArtistProfile id={params.id} />
-}
--- frontend-nextjs/app/terms/page.tsx
+++
@@ -1,5 +0,0 @@
-import Terms from '@/components/pages/Terms'
-
-export default function TermsPage() {
-  return <Terms />
-}
